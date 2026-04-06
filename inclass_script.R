0d22adbecdd1dcd5c0d8e39ee7b4517c4469fb9f
Paige Cole <paigejc25@vt.edu>
2026-04-06 11:24:15 -0400

    installing network packages

--- inclass_script.R
+++ inclass_script.R
@@ -2,3 +2,13 @@
 penguins 
 ggplot (data = penguins)
 3 + 3
+install.packages("sf")
+install.packages('terra', repos= 'https://rspatial.r-universe.dev')
+install.packages("tidyterra")
+install.packages("USAboundaries", repos = c("https://ropensci.r-universe.dev"))
+install.packages("USAboundariesData", repos = c("https://ropensci.r-universe.dev"))
+install.packages("rnaturalearth")
+install.packages('tidygeocoder')
+install.packages("elevatr")
+if (!require('devtools')) install.packages('devtools')
+devtools::install_github('rstudio/leaflet')
